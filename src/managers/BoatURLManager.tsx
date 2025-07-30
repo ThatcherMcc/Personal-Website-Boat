6d00be36811e464fea01b21d97e33aaeda4ecf0d
Thatcher McClure <thatchermcclure@gmail.com>
2025-07-30 02:18:23 -0500

    refactor: Relocate select manager files from components to src/managers

---
+++ src/managers/BoatURLManager.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+export type BoatState = "exterior" | "interior";
+export type BoatRoom = "captains-quarters" | "treasure-room" | null;
+
+export default function BoatStateURLManager() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const updateBoatState = (newState: BoatState) => {
+    const newSearchParams = new URLSearchParams(searchParams.toString());
+    newSearchParams.set("boatState", newState);
+    if (newState == "exterior") {
+      newSearchParams.delete("room");
+    }
+    router.push(`?${newSearchParams}`, { scroll: false });
+  };
+
+  const updateRoom = (newRoom: BoatRoom) => {
+    const newSearchParams = new URLSearchParams(searchParams.toString());
+    if (newRoom) {
+      newSearchParams.set("room", newRoom);
+    } else {
+      newSearchParams.delete("room");
+    }
+    router.push(`?${newSearchParams}`, { scroll: false });
+  };
+
+  useEffect(() => {
+    const boatState =
+      (searchParams.get("boatState") as BoatState) || "exterior";
+    console.log(boatState);
+
+    if (!boatState) {
+      updateBoatState("exterior");
+      return;
+    }
+
+    const handleBoatClick = () => {
+      if (boatState != "interior") {
+        updateBoatState("interior");
+      }
+    };
+
+    const handleRoomClick = (event: CustomEvent) => {
+      updateRoom(event.detail.room);
+    };
+
+    const handleBackToSea = () => {
+      updateBoatState("exterior");
+    };
+
+    window.addEventListener("boatClick", handleBoatClick);
+    window.addEventListener("roomClick", handleRoomClick as EventListener);
+    window.addEventListener("backToSea", handleBackToSea);
+
+    return () => {
+      window.removeEventListener("boatClick", handleBoatClick);
+      window.removeEventListener("roomClick", handleRoomClick as EventListener);
+      window.removeEventListener("backToSea", handleBackToSea);
+    };
+  }, [router, searchParams]);
+
+  return null;
+}
